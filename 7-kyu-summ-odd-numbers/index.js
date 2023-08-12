0dfafed36660f4805681b5b61930c418b2dc3796
dmitriibutakov <dmitriidevelopment@gmail.com>
2023-08-12 22:45:10 +0400

    init

---
+++ 7-kyu-summ-odd-numbers/index.js
@@ -0,0 +1,26 @@
+// 7 kyu
+// Sum of odd numbers
+//
+// 294155484% of 12,77040,465 of 134,921hhelwich
+// JavaScript
+// TRAINNEXT KATA
+// Details
+// Solutions
+// Forks (24)
+// Discourse (655)
+// Collect|
+// DESCRIPTION:
+//
+// Given the triangle of consecutive odd numbers:
+//
+//     1
+// 3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+// ...
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+}
+
+console.log(rowSumOddNumbers(27))
